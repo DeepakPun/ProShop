2f4af8f029989f007b0b402f77e1406fbf63f827
DeepakPun <90846395+DeepakPun@users.noreply.github.com>
2023-07-01 22:27:10 -0400

    Completed Section 4 mongo DB and mongoose

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+const port = process.env.PORT || 5000;
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API Is Running....');
+});
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
