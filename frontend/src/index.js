490ec2dd240f75f48d557c3b548c14072850c860
DeepakPun <90846395+DeepakPun@users.noreply.github.com>
2023-07-09 11:22:24 -0400

    Completed Section 8 FrontEnd Authentication. Fixed error on registration route. Earlier, I forgot to add the method and body. The url was also going to USERS_URL/register instead of USERS_URL.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 import {
   createBrowserRouter,
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 );
